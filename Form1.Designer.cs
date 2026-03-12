08eb1f860f9055d3abc9219a2faa037029585fc9
d10F-xy <f01d_@naver.com>
2026-03-12 11:47:40 +0900

    2교시 종료. 버튼의 위치 값 세팅 및 버튼을 새로운 위치로 옮기는 명령어 공부

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,34 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            Target = new Button();
             SuspendLayout();
             // 
-            // button1
+            // Target
             // 
-            button1.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            button1.ForeColor = SystemColors.ActiveCaption;
-            button1.Location = new Point(317, 155);
-            button1.Name = "button1";
-            button1.Size = new Size(152, 66);
-            button1.TabIndex = 0;
-            button1.Text = "나를 잡아봐";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            Target.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            Target.ForeColor = Color.Red;
+            Target.Location = new Point(392, 160);
+            Target.Name = "Target";
+            Target.Size = new Size(152, 66);
+            Target.TabIndex = 0;
+            Target.Text = "나를 잡아봐";
+            Target.UseVisualStyleBackColor = true;
+            Target.Click += Target_Click;
+            Target.MouseEnter += Target_MouseEnter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(Target);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Catch the button version 1.2";
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
+        private Button Target;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,8 +6,18 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+        private void Target_MouseEnter(object sender, EventArgs e)
+        {
+            // 새로운 위치 값 세팅. 새로운 X, Y 좌표값
+            int x_position = 100;
+            int y_position = 150;
 
-        private void button1_Click(object sender, EventArgs e)
+            // 버튼을 새로운 위치로 옮김 (새로운 Point 객체 생성)
+            Target.Location = new Point(x_position, y_position); 여기서 Target은 버튼 이름이니 버튼의 이름에 맞게 설정하면 됨
+
+           
+
+        private void Target_Click(object sender, EventArgs e)
         {
 
         }
