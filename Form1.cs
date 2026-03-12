9b27de7bea5a36bca16607558b2c8bb69cb3ddf1
d10F-xy <f01d_@naver.com>
2026-03-12 11:51:56 +0900

    1단계 Task 완료

--- Form1.cs
+++ Form1.cs
@@ -9,13 +9,34 @@ namespace CatchButton
         private void Target_MouseEnter(object sender, EventArgs e)
         {
             // 새로운 위치 값 세팅. 새로운 X, Y 좌표값
-            int x_position = 100;
-            int y_position = 150;
+            //int x_position = 100;
+            //int y_position = 150;
 
             // 버튼을 새로운 위치로 옮김 (새로운 Point 객체 생성)
-            Target.Location = new Point(x_position, y_position); 여기서 Target은 버튼 이름이니 버튼의 이름에 맞게 설정하면 됨
+            // Target.Location = new Point(x_position, y_position); 여기서 Target은 버튼 이름이니 버튼의 이름에 맞게 설정하면 됨
 
-           
+            // 1. 난수생성기준비
+            Random rd = new Random();
+
+            // 2. 가용 영역 계산 (버튼이 폼 테두리에 걸리지 않게 보호)
+            // ClientSize는 타이틀 바와 테두리를 제외한실제흰도화지영역임
+            int maxX = this.ClientSize.Width - Target.Width; // 버튼의 가로길이만큼 빼줘야 버튼이 폼의 오른쪽 테두리를 넘지 않음
+            int maxY = this.ClientSize.Height - Target.Height; // 버튼의 세로길이만큼 빼줘야 버튼이 폼의 아래쪽 테두리를 넘지 않음
+
+            // 3. 랜덤좌표추출(0 ~ 최대가용치사이)
+            int nextX = rd.Next(0, maxX);
+            int nextY = rd.Next(0, maxY);
+
+            // 4. 위치할당(새로운Point 객체생성)
+            Target.Location = new Point(nextX, nextY);
+
+            // 5. 시각적피드백(폼제목표시줄에좌표출력)
+            this.Text = $"버튼위치: ({nextX}, {nextY})"; // This는 현재 폼 객체 자신을 가리킴. Text는 폼 제목 표시줄에 출력되는 문자열을 의미
+        }
+
+
+
+
 
         private void Target_Click(object sender, EventArgs e)
         {
